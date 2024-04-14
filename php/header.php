8cd95291628f6fda9d20e145422b10e39b665015
MCelian <celianmignot@yahoo.fr>
2024-04-14 20:24:22 +0200

    Ajout de la mise en page finale

--- php/header.php
+++ php/header.php
@@ -1,33 +1,33 @@
 <?php
 include_once('../php/data.php');
 ?>
+
 <header>
-    <a href="../php/index.php">
-        <img src="../img/logo.jpg" alt="Logo du site" id="logo">
-    </a>
-    <div id="banderole">
-        <h1 id="titre">Société Néomania</h1>
+    <div class="logo_titre_connexion">
+        <a href="../php/index.php" id="logo_header"> <img src="../img/switch.jpg" alt="Logo du site" id="logo"></a>
+        <h1 id="titre_header">Néo-Mania</h1>
+        <div id="panier_connexion">
+            <?php if(!empty($_SESSION["user"])){
+                echo "<span id='nom_user'>".$_SESSION['user']['nom']." ".$_SESSION['user']['prenom']."</span>";
+                echo "<form action='form.php' method='post'>
+                        <input   type='submit' value='Se Déconnecter' class='bouton_client'>
+                        <input type='hidden' name='action' value='deconnexion'>
+                        </form>";
+            }  
+            else{
+                echo "<form action='login.php' method='post'>
+                    <input  type='submit' value='Se Connecter' class='bouton_client'>
+                    </form>";
+            }
+        ?>
+            <a href="../php/panier.php"><button type="button" class="bouton_client"> Panier</button></a>
+        </div>
+    </div>
+    <div class="categories">
         <ul>
             <li><a href="../php/index.php">Accueil</a></li>
             <?php afficherMenu(); ?>
             <li><a href="../php/contact.php">Contact</a></li>
         </ul>
-    </div>
-    <div id="espaceClient">
-        <?php if(!empty($_SESSION["user"])){
-            echo $_SESSION['user']['nom']." ".$_SESSION['user']['prenom'];
-            echo "<form action='form.php' method='post'>
-                    <input  type='submit' value='Se Déconnecter'>
-                    <input type='hidden' name='action' value='deconnexion'>
-                    </form>";  
-        }  
-        else{
-            echo "<form action='login.php' method='post'>
-                    <input  type='submit' value='Se Connecter'>
-                    </form>";
-        }
-        ?>
-        <br>
-        <a href="../php/panier.php">Panier</a>
     </div>
 </header>
